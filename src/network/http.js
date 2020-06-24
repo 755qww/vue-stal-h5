7083a6c58a9c47d29afdad78b63f98c6b25a5218
755qww <1398454124@qq.com>
2020-06-24 12:07:57 +0800

    update

--- src/network/http.js
+++ src/network/http.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import apiContent from './api'
-import { is } from 'core-js/fn/object'
 
 // 新建一个实例来使用axios
 let instance = axios.create({
@@ -8,28 +7,17 @@ let instance = axios.create({
   timeout: 10000
 })
 
-const Http = {}
-for (let key in apiContent){
-  let api = apiContent[key]  // 遍历出所有定义的 api（url，method）
-  /**
-   *  @params:  请求参数 get post 等
-   *  @isFormDate: 标识是否为form-date请求
-   *  @config: 配置参数
-   */
-  Http[key] = async function(params, isFormDate = false, config = {}){
-    let newParams = {}
-    if(params&& is){
-      
-    }
-  }
-}
 
-// 添加请求拦截器
+
+
+// 添加请求拦截器  ==>  对请求参数进行处理
 instance.interceptors.request.use(config => {
   // 在发送http请求之前做什么
   let token = localStorage.getItem('token')
   if(token){
     config.headers['X-Access-Token'] = token
+    config.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+    
   }
   return config
 },error => {
@@ -46,4 +34,5 @@ instance.interceptors.response.use(response => {
   console.log(err)
 })
 
+// export default Http
 
